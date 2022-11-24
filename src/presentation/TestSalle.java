0f6e368a303a8366b42482b0feb93c9abf651a6f
Samih saad <116285682+Bakka08@users.noreply.github.com>
2022-11-24 09:47:55 +0100

    first commit

---
+++ src/presentation/TestSalle.java
@@ -0,0 +1,47 @@
+package presentation;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import entities.Machine;
+import entities.Salle;
+import services.MachineService;
+import services.SalleService;
+
+public class TestSalle {
+
+	public static void main(String[] args) {
+
+		SalleService ss = new SalleService();
+//		ss.create(new Salle("CC2", "Info"));
+		// ss.create(new Salle("LR", "Auto"));
+
+//		System.out.println("Liste des salles disponibles  : ");
+//		for(Salle s : ss.findAll())
+//			System.out.println(s);
+////		
+//		ss.delete(ss.findById(1));
+
+		MachineService ms = new MachineService();
+//		ms.create(new Machine("PO45", "HP", 10000, new Date(), ss.findById(2)));
+//		ms.create(new Machine("RT45", "Lenovo", 7000, new Date(), ss.findById(2)));
+//		ms.create(new Machine("CC45", "Deel", 9000, new Date(), ss.findById(3)));
+
+		//for (Salle s1 : ss.findAll()) {
+			//System.out.println(s1 + " \n ses machines : ");
+			//for (Machine m : ms.findAll()) {
+				//if (m.getSalle().getId() == s1.getId())
+					//System.out.println("\t" + m);
+			//}
+		//}
+		
+		DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+		Date d1 = new Date("2022/11/15");
+		Date d2 = new Date("2022/11/25");
+		System.out.println(ms.machinebetweendates(d1, d2));
+		//Salle s1 = new Salle("Auto");
+		//System.out.println(ms.machinsalle(s1));
+	}
+	
+}
